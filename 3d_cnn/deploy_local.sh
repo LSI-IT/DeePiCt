be8930e233c7adc96119464c91208dfeefe076bf
Christopher Lilienthal <lilienc@umich.edu>
2024-10-16 12:56:58 -0400

    Prepend to PYTHONPATH

--- 3d_cnn/deploy_local.sh
+++ 3d_cnn/deploy_local.sh
@@ -3,7 +3,7 @@
 export srcdir=$(realpath ${BASH_SOURCE[0]} | xargs dirname)
 config_file=$1
 echo CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
-export PYTHONPATH=${srcdir}/src
+export PYTHONPATH=${srcdir}/src:$PYTHONPATH
 echo PYTHONPATH=$PYTHONPATH
 
 snakemake \
