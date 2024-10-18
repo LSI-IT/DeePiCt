999efb71026b0429925d544a07bd129677037541
Christopher Lilienthal <lilienc@umich.edu>
2024-10-18 12:50:45 -0400

    Load the deepict module and some formatting

--- 3d_cnn/deploy_cluster.sh
+++ 3d_cnn/deploy_cluster.sh
@@ -5,16 +5,17 @@ config_file=$1
 export PYTHONPATH=${srcdir}/src:$PYTHONPATH
 echo PYTHONPATH=$PYTHONPATH
 
+module load deepict/1.0.0
 module load cuda/12.2.2
 
 srun -t 10:00:00 -c 1 --mem 4G \
-    snakemake \
-    --snakefile "${srcdir}/snakefile" \
-    --cluster "sbatch" \
-    --config config="${config_file}" gpu=$CUDA_VISIBLE_DEVICES \
-    --jobscript "${srcdir}/jobscript.sh" \
-    --jobs 20 \
-    --printshellcmds \
-    --latency-wait 30 \
-    --max-jobs-per-second 1 \
-    --max-status-checks-per-second 0.1 
+snakemake \
+--snakefile "${srcdir}/snakefile" \
+--cluster "sbatch" \
+--config config="${config_file}" gpu=$CUDA_VISIBLE_DEVICES \
+--jobscript "${srcdir}/jobscript.sh" \
+--jobs 20 \
+--printshellcmds \
+--latency-wait 30 \
+--max-jobs-per-second 1 \
+--max-status-checks-per-second 0.1
