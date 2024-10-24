b00c78ca66f8785e6ad402bf8d30f6e34159c50e
Christopher Lilienthal <lilienc@umich.edu>
2024-10-24 15:12:23 -0400

    Change nodes to ntasks and add a nodes parameter so we can get gpus on the same machine

--- 2d_cnn/jobscript.sh
+++ 2d_cnn/jobscript.sh
@@ -5,17 +5,18 @@
 
 #SBATCH --mem={job.params.memory}
 #SBATCH --time={job.params.walltime}
-#SBATCH --ntasks={job.params.nodes}
+#SBATCH --ntasks={job.params.ntasks}
 #SBATCH --cpus-per-task={job.params.cores}
+#SBATCH --nodes={job.params.nodes}
 {job.params.gres}
 
 module purge
 module load deepict/1.0.0
+
 export DISPLAY=0.0
 export PYTHONUNBUFFERED=1
 
 echo CLUSTER NAME: $SLURM_CLUSTER_NAME
 echo CLUSTER NODE: $SLURMD_NODENAME
-echo CONDA ENV: $CONDA_DEFAULT_ENV
 
 {exec_job}
